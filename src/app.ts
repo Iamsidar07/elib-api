89974d3c906120febcff99d9994bc677d2c1d1da
iamsidar07 <ms8460149@gmail.com>
2024-04-11 08:15:33 +0530

    add global error handler & env example

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
 
-const app = express()
+const app = express();
 
 // routing
 app.get("/", (req, res, next) => {
-    res.json({message: "hello"})
-})
+  const error = createHttpError(400, "Something went wrong");
 
-export default app
+  throw error;
+  res.json({ message: "hello" });
+});
+
+// global error handler at bottom
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = error.statusCode || 500;
+  res.status(statusCode).json({
+    message: error.message,
+    errorStack: config.env === "development" ? error.stack : "",
+  });
+});
+
+export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,11 @@
-import {config as dotenvConfig} from "dotenv"
-dotenvConfig()
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
 
 const _config = {
-    port: process.env.PORT,
-    dbUri: process.env.MONGODB_URI
-}
+  port: process.env.PORT,
+  dbUri: process.env.MONGODB_URI,
+  env: process.env.NODE_ENV,
+};
 
 // make _config readonly
-export const config = Object.freeze(_config)
+export const config = Object.freeze(_config);
