31294ed77f357bdf2851ca7058cfeb0169049dbb
iamsidar07 <ms8460149@gmail.com>
2024-04-11 10:46:23 +0530

    add book router

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
 
 // routing
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 
 // global error handler at bottom
 app.use(globalErrorHandler);
